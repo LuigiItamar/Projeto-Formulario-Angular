8da2bca0301b5c97a1e063ff7fac05b5d1e3748a
Luigi Fidêncio Itamar <luigifidencioitamar@gmail.com>
2025-08-20 18:19:46 -0300

    Estilização das Paginas

--- cadastro-usuarios/src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
+++ cadastro-usuarios/src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
@@ -3,11 +3,24 @@ import { FormBuilder, FormGroup, Validators, FormArray, ReactiveFormsModule } fr
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatError } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-cadastro-usuario',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './cadastro-usuario.component.html',
   styleUrls: ['./cadastro-usuario.component.css']
 })
